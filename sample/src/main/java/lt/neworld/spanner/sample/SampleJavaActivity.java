c591d42b506d72cbe373c5060606cfb2a23fffb5
Andrius Semionovas <aneworld@gmail.com>
2017-08-01 06:26:41 +0300

    Initial setup

---
+++ sample/src/main/java/lt/neworld/spanner/sample/SampleJavaActivity.java
@@ -0,0 +1,13 @@
+package lt.neworld.spanner.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SampleJavaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+    }
+}
