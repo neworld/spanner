b5a77612da92637ab2cff0048ff988c302e5d437
antoxa2584 <antoxa2584@gmail.com>
2017-10-02 19:37:41 +0300

    Removed unused recourses Provided kotlin sample Moved all lib to kotlin

--- sample/src/main/java/lt/neworld/spanner/sample/SampleJavaActivity.java
+++ sample/src/main/java/lt/neworld/spanner/sample/SampleJavaActivity.java
@@ -1,21 +1,16 @@
 package lt.neworld.spanner.sample;
 
 import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Spannable;
 import android.text.method.LinkMovementMethod;
-import android.text.style.StyleSpan;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import lt.neworld.spanner.SpanBuilder;
 import lt.neworld.spanner.Spanner;
 import lt.neworld.spanner.Spans;
-
-import static lt.neworld.spanner.Spans.*;
 
 public class SampleJavaActivity extends AppCompatActivity {
 
@@ -35,41 +30,30 @@ public class SampleJavaActivity extends AppCompatActivity {
             }
         };
 
-        // @formatter:off
         Spannable spannable = new Spanner()
                 .append("Original text\n\n")
-                .append("big\n", Spans.sizePX(100))
-                .append("big in DP\n", sizeDP(30))
-                .append("50% of original size\n", scaleSize(0.5f))
-                .append("bold\n", bold())
-                .append("italic\n", italic())
-                .append("bold and italic\n", boldItalic())
-                .append("custom typeface\n", font("sans-serif-black"))
-                .append("strike through\n", strikeThrough())
-                .append("underline\n", underline())
-                .append("background\n", background(Color.YELLOW))
-                .append("foreground\n", foreground(Color.RED))
-                .append("subscript\n", subscript())
-                .append("superscript\n", superscript())
-                .append(" \n", image(getResources().getDrawable(R.drawable.ic_android_16dp)))
-                .append("quite\n", quote())
-                .append("The quick brown fox jumps over the lazy dog\n", bold(), foreground(0xFF904f1c), Spans.quote())
-                .span("fox", foreground(Color.RED))
-                .span("dog", foreground(Color.RED))
-                .append("Custom\n", custom(new CustomSpan()))
-                .append("Click here\n", click(onClickListener))
-                .append("http://www.android.com\n", url("http://www.android.com"))
-                ;
-        // @formatter:on
+                .append("big\n", Spans.INSTANCE.sizePX(100))
+                .append("big in DP\n", Spans.INSTANCE.sizeDP(30))
+                .append("50% of original size\n", Spans.INSTANCE.scaleSize(0.5f))
+                .append("bold\n", Spans.INSTANCE.bold())
+                .append("italic\n", Spans.INSTANCE.italic())
+                .append("bold and italic\n", Spans.INSTANCE.boldItalic())
+                .append("custom typeface\n", Spans.INSTANCE.font("sans-serif-black"))
+                .append("strike through\n", Spans.INSTANCE.strikeThrough())
+                .append("underline\n", Spans.INSTANCE.underline())
+                .append("background\n", Spans.INSTANCE.background(Color.YELLOW))
+                .append("foreground\n", Spans.INSTANCE.foreground(Color.RED))
+                .append("subscript\n", Spans.INSTANCE.subscript())
+                .append("superscript\n", Spans.INSTANCE.superscript())
+                .append(" \n", Spans.INSTANCE.image(getResources().getDrawable(R.drawable.ic_android_16dp)))
+                .append("quite\n", Spans.INSTANCE.quote())
+                .append("The quick brown fox jumps over the lazy dog\n", Spans.INSTANCE.bold(), Spans.INSTANCE.foreground(0xFF904f1c), Spans.INSTANCE.quote())
+                .span("fox", Spans.INSTANCE.foreground(Color.RED))
+                .span("dog", Spans.INSTANCE.foreground(Color.RED))
+                .append("Custom\n", Spans.INSTANCE.custom(new CustomSpan()))
+                .append("Click here\n", Spans.INSTANCE.click(onClickListener))
+                .append("http://www.android.com\n", Spans.INSTANCE.url("http://www.android.com"));
 
         textView.setText(spannable);
     }
 }
-
-class CustomSpan implements SpanBuilder {
-    @Override
-    public Object build() {
-        return new StyleSpan(Typeface.BOLD);
-    }
-}
-
